82f215df3505ad9462d12608af0ed91bf6494b34
PalchukDmytro <palya1984@gmail.com>
2024-01-13 00:35:26 +0200

    Update task-3.js

--- js/task-3.js
+++ js/task-3.js
@@ -6,9 +6,7 @@ function getElementWidth(content, padding, border) {
 
     return contentWidth + paddingWidth * 2 + borderWidth * 2;
 
-};
-
-getElementWidth();
+}
 
 console.log(getElementWidth("50px", "8px", "4px")); // 74
 console.log(getElementWidth("60px", "12px", "8.5px")); // 101
